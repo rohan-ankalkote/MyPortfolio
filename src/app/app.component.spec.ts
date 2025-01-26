1c92745ae1880d746f87ffc2d153c78ea1656d88
Rohan Ankalkote <rohanankalkote@gmail.com>
2025-01-26 20:10:52 +0530

    Add Banner

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MyPortfolio' title`, () => {
+  it(`should have the 'Portfolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyPortfolio');
+    expect(app.title).toEqual('Portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MyPortfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Portfolio');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BannerComponent } from './banner/banner.component';
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [BannerComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    host: {
+        '[class.pc]': 'isPc'
+    }
 })
-export class AppComponent {
-  title = 'MyPortfolio';
+export class AppComponent implements OnInit {
+    title = 'Rohan Ankalkote';
+    isPc: boolean = false;
+
+    constructor(private breakPointObserver: BreakpointObserver) {}
+
+    ngOnInit(): void {
+        this.breakPointObserver.observe([Breakpoints.WebLandscape]).subscribe({
+            next: result => this.isPc = result.matches
+        })
+    }
 }
